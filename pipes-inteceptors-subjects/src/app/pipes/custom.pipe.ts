6e0f094568f5e1d3244eb79fba4147a33ad6511d
AleksandarBoev <aleksandarboev95@gmail.com>
2024-11-17 15:59:39 +0200

    Add simple demo for custom pipe

---
+++ pipes-inteceptors-subjects/src/app/pipes/custom.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'excite'
+})
+export class CustomPipe implements PipeTransform {
+  /**
+   * First parameter is always the thing, which is on the left side of the pipe.
+   * In this case, it is "value" of type "string". Which means this pipe is meant to be used
+   * for strings.
+   *
+   * Every other parameter is whatever I want them to be. In the html, these parameters are passed
+   * via double dots ":". To pass many parameters, just add many ":".
+   */
+  transform(value: string, exclamationCount: number = 1, questionMarkCount: number = 0): string {
+    if (!value) return '';
+    const exclamations = '!'.repeat(exclamationCount);
+    const questionMarks = '?'.repeat(questionMarkCount);
+    return value.toUpperCase() + exclamations + questionMarks;
+  }
+}
